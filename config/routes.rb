a18c539aeb194172b1d5454cb358cdad27f4ea03
Jeff <jpkalbacher@gmail.com>
2015-11-20 15:19:28 -0800

    simply user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
