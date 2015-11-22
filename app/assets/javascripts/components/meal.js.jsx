541605a9fbe649a6d49f2804261180f6adaae641
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-22 10:34:15 -0800

    Adds Meals route and dummy component

---
+++ app/assets/javascripts/components/meal.js.jsx
@@ -0,0 +1,12 @@
+Meal = React.createClass({
+  render: function(){
+    return (
+      <div className="container">
+        <div className="row text-center">
+          Here is a Meal.
+        </div>
+      </div>
+    );
+
+  }
+});
