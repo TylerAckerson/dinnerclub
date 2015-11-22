1ce75f9a0db61f9500abc845f89bbedd5d3096b4
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-22 10:57:13 -0800

    Adds dummy attendee component

---
+++ app/assets/javascripts/components/attendee.js.jsx
@@ -0,0 +1,10 @@
+Attendee = React.createClass({
+
+  render: function(){
+    return (
+      <div>
+        Attendee...
+      </div>
+    );
+  }
+});
--- app/assets/javascripts/components/meal.js.jsx
+++ app/assets/javascripts/components/meal.js.jsx
@@ -1,12 +1,18 @@
 Meal = React.createClass({
+  dummyMeal: function(){
+    return ['tyler@tyler.tyler', 'jeff@jeff.jeff', 'andrew@andrew.andrew'];
+  },
+
   render: function(){
     return (
       <div className="container">
-        <div className="row text-center">
-          Here is a Meal.
+        <div className="row text-center"> {
+          this.dummyMeal().map(function(attendee){
+            return <Attendee attendee={attendee} />;
+          })
+        }
         </div>
       </div>
     );
-
   }
 });
