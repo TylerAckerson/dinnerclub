5d4d8b147f55a3fbb293aafae2f90584a8538a1c
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-22 11:23:43 -0800

    Adds component styling and form to add attendee

--- app/assets/javascripts/components/meal.js.jsx
+++ app/assets/javascripts/components/meal.js.jsx
@@ -7,13 +7,19 @@ Meal = React.createClass({
     return (
       <div className="container text-center">
         <div className="row">
-          <h2> Meal </h2>
-        </div>
-        <div className="row"> {
-          this.dummyMeal().map(function(attendee){
-            return <Attendee attendee={attendee} />;
-          })
-        }
+          <div className="col-sm-6 col-sm-offset-3 meal">
+            <h3 className="header">Your meal</h3>
+
+            <AddAttendee />
+
+            <h4>Attendee list</h4>
+            <div className="row"> {
+              this.dummyMeal().map(function(attendee){
+                return <Attendee key={attendee} attendee={attendee} />;
+              })
+            }
+            </div>
+          </div>
         </div>
       </div>
     );
