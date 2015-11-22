4f4c66aee5c8e18859021eef096dad9155a14e3d
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-22 11:52:58 -0800

    Adds remove button and formats attendees as table

--- app/assets/javascripts/components/attendee.js.jsx
+++ app/assets/javascripts/components/attendee.js.jsx
@@ -1,10 +1,24 @@
 Attendee = React.createClass({
+  removeAttendee: function(e) {
+    e.preventDefault();
+
+    setTimeout(function() {
+      // OrderActions.orderRemoveItem(this.props.orderItem.counter);
+    }.bind(this), 500 );
+  },
 
   render: function(){
     return (
-      <div>
-        <p>{this.props.attendee}</p>
-      </div>
+      <tr className="row">
+        <td className="col-xs-6 col-xs-offset-2 text-left">{this.props.attendee}</td>
+        <td className="col-xs-2">
+          <button type="submit"
+          className="btn btn-default full-width remove"
+          onClick={this.removeAttendee}>
+            <span className="glyphicon glyphicon-minus"/>
+          </button>
+        </td>
+      </tr>
     );
   }
 });
