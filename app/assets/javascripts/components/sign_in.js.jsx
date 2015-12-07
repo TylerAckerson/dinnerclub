c69970253c0ff17e4f597d51bb9f30f580f10e24
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-12-07 15:00:35 -0800

    Removes sign up success border, fixes col-widths

--- app/assets/javascripts/components/sign_in.js.jsx
+++ app/assets/javascripts/components/sign_in.js.jsx
@@ -34,7 +34,7 @@ SignIn = React.createClass({
     return (
       <div>
       <div className="row text-center top-buffer sign-in">
-        <div className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
+        <div className="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-6 col-lg-offset-3">
           <h3 className="header">Enter your email:</h3>
           <form role="form" className="form-inline"
                             onSubmit={this.handleEmail}>
@@ -55,8 +55,11 @@ SignIn = React.createClass({
       </div>
 
       <div className="row text-center">
-        <div id="sign-up-success" className="col-xs-4 col-xs-offset-4 col-sm-4 col-sm-offset-4 col-md-2 col-md-offset-5 col-lg-2 col-lg-offset-5 hide">
-          <p>You&#39;ve been placed on the waiting list!</p>
+
+        <div id="sign-up-success" className="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4 hide">
+          <div className="sign-up-success-border-top"/>
+            <p>You&#39;ve been placed on the waiting list!</p>
+          <div className="sign-up-success-border-bottom"/>
         </div>
       </div>
 
